e5a3d7cae13514761f7e45a9318fbbd7b35648bb
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-06-30 02:05:56 -0300

    Update and rename admin-style.css to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHIc2E4XwRO5FXo4uHlTQVRArOis73MjE",
+  authDomain: "projeto-deus-yato-928-sk-default-rtdb.firebaseapp.com",
+  databaseURL: "https://projeto-deus-yato-928-sk-default-rtdb.firebaseio.com",
+  projectId: "projeto-deus-yato-928-sk-default-rtdb",
+  storageBucket: "projeto-deus-yato-928-sk-default-rtdb.appspot.com",
+  messagingSenderId: "790408726854",
+  appId: "1:790408726854:android:e2f0de7b7d5dba96b0fd47"
+};
+firebase.initializeApp(firebaseConfig);
